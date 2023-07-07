c41efaf9647cd9d85aed62b2fcbf6d66f39ea502
AichaMbongo <zindamoyen2@gmail.com>
2023-07-07 19:30:56 +0300

    1st commit

---
+++ src/header.php
@@ -0,0 +1,28 @@
+<!-- header.php -->
+<header>
+<header>
+      <nav>
+          <div class="logo">
+              <a href="index.html"><img style="width: 9%; border-radius: 10%; margin-top: 0%;" src="img/J4U logo.png" alt="Rosa Logo"></a>
+          </div>
+          <div class="toggle">
+              <span class="first"></span>
+              <span class="middle"></span>
+              <span class="last"></span>
+          </div>
+          <div class="navigation-bar">
+              <ul>
+                  <li class="active"><a href="index.html">Home<span class="underline"></span></a></li>
+                  <li><a href="#">Reservations<span class="underline"></span></a></li>
+                  <li><a href="#">Menu<span class="underline"></span></a></li>
+                  <li><a href="#">Blog<span class="underline"></span></a></li>
+                  <li><a href="#">Features<span class="underline"></span></a></li>
+                  <li><a href="#">Shop<span class="underline"></span></a></li>
+                  <li><a href="#">Contact<span class="underline"></span></a></li>
+              </ul>
+          </div>
+      </nav>
+    
+  </header>
+  <!-- Your header content here -->
+</header>
